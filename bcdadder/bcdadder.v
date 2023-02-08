66dcc21f33586ba143b9634b8417b3cedccf02d8
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2023-02-08 08:57:47 +0530

    Add files via upload

--- bcdadder/bcdadder.v
+++ bcdadder/bcdadder.v
@@ -1,5 +1,4 @@
-
-module full_adder(a,b,c,s,co);
+module fulladder(a,b,c,s,co);
 input a,b,c;
 output s,co;
 assign s=a^b^c;
@@ -12,28 +11,29 @@ input [3:0] x,y;
 input cin;
 output [3:0] sum;
 output cout;
+output[3:0] carryout;
 wire c0,c1,c2;
 
-full_adder fa0(.a(x[0]),.b(y[0]),.c(cin),.s(sum[0]),.co(c0));
-full_adder fa1(.a(x[1]),.b(y[1]),.c(c0),.s(sum[1]),.co(c1));
-full_adder fa2(.a(x[2]),.b(y[2]),.c(c1),.s(sum[2]),.co(c2));
-full_adder fa3(.a(x[3]),.b(y[3]),.c(c2),.s(sum[3]),.co(cout));
+fulladder fa0(.a(x[0]),.b(y[0]),.c(cin),.s(sum[0]),.co(c0));
+fulladder fa1(.a(x[1]),.b(y[1]),.c(c0),.s(sum[1]),.co(c1));
+fulladder fa2(.a(x[2]),.b(y[2]),.c(c1),.s(sum[2]),.co(c2));
+fulladder fa3(.a(x[3]),.b(y[3]),.c(c2),.s(sum[3]),.co(cout));
 endmodule
-module BCD_adder(S, C, A, B, C0);
-input [3:0] A, B;
-input C0;
-output [3:0] S;
-output C;
-wire C1, C2, C3, C4, C5;
-wire [3:0]X, Z;
-and (C1, Z[3], Z[2]);
-and (C2, Z[3], Z[1]);
-or (C, C3, C1,C2);
-assign X[2] = C;
-assign X[1] = C;
-assign X[3] = C5;
-assign X[0] = C5;
-//four bit adder body from instance of full_adder
-four_bit_adder F_1 ( A, B, C0,Z,C3);
-four_bit_adder F_2 (X, Z, C0,S,C4);
-endmodule 
+
+module bcdadder(A,B,c,S,Cout);
+input [3:0]A,B;
+output S;
+input c;
+output s1,s2,s3;
+output Cout;
+wire [3:0]sum1_w;
+wire a1,a2;
+	wire cout1_w;
+	wire combo_out;
+	assign a1= sum1_w[3]&sum1_w[2];
+	assign a2= sum1_w[3]&sum1_w[1];
+	assign combo_out=a1|a2|cout1_w;
+
+four_bit_adder fa1(.x(A),.y(B),.cin(c),.sum(sum1_w),.cout(cout1_w));
+four_bit_adder fa2(.x(sum1_w),.y({0,combo_out ,combo_out , 0}),.cin(cout1_w),.sum(S),.cout(Cout));
+endmodule
