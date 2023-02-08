6aab9f8944f8a57bc7f678891130a43b93fcc9df
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2023-02-08 09:14:20 +0530

    Create mux4_1.v

---
+++ mux4_1.v
@@ -0,0 +1,19 @@
+
+module mux41(out, in, s);
+
+input [3:0] in;
+input s;
+
+output reg out;
+
+always @(*)
+begin
+	case(s)
+	
+		2'b00 : out = in[0];
+		2'b01 : out = in[1];
+		2'b10 : out = in[2];
+		2'b11 : out = in[3];
+	endcase
+end
+	endmodule
