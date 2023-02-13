aa37efd489c876aa8dd330e187a89c9c15854edc
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2023-02-14 00:24:38 +0530

    Create tb.v

---
+++ 53.Decadecounter/tb.v
@@ -0,0 +1,21 @@
+module decadecounter_tb;
+wire [3:0] count;
+reg en,clock;
+  
+decadecounter dc(.en(en), .clock(clock), .count(count));
+initial begin
+
+    en=0;
+    clock=0;
+   #10 en=1'd1;
+end
+  
+  always
+    #5 clock=~clock; 
+
+   initial
+    begin
+      #10000$finish;
+    end
+  
+endmodule
