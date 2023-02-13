8386a20610bdd8ce95341017fe70e80572857440
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2023-02-14 00:36:52 +0530

    Create tb.v

---
+++ 55. MasterslaveJK flipflop/tb.v
@@ -0,0 +1,26 @@
+module masterslave_tb;
+  reg s;
+	reg r;
+	reg clk;
+	wire qn;
+	wire qn_bar;
+	masterslave uut (
+		.s(s), 
+		.r(r), 
+		.clk(clk), 
+		.qn(qn), 
+		.qn_bar(qn_bar)
+	);
+	initial begin
+	clk=0;
+	#10 s = 0;r = 0;
+	#10 s = 0;r = 1;
+   #10 s = 1;r = 0;
+   #10 s = 1;r = 1;	
+	#10 s = 0;r = 0;
+   #10 s = 0;r = 1;
+	end
+	always #5 clk=~clk;
+
+endmodule
+
