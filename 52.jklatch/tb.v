118202692d30e1b10cf9589d7c4e815c3c441b70
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2023-02-14 00:17:12 +0530

    Create tb.v

---
+++ 52.jklatch/tb.v
@@ -0,0 +1,16 @@
+module jk_latch_tb;
+  reg j, k, clk;
+  wire q;
+jklatch dut (j, k, clk, q);
+
+  initial begin
+    j = 0; k = 0; clk = 0;
+    #5 j = 1; k = 0; clk = 1;
+    #5 j = 0; k = 1; clk = 1;
+    #5 j = 1; k = 1; clk = 1;
+    #5 clk = 0;
+    #5 $finish;
+  end
+
+  always #5 clk = ~clk;
+endmodule
